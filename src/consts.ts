ef2222a3c520a9bc9613b684efaa9c1d67f80114
iukconnor0942-sudo <iukconnor0942@gmail.com>
2026-04-12 18:31:58 +0800

    fix: 强制添加日志页面和移动端优化

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,8 @@
+// 这里的文字你可以根据喜好修改，但变量名一定要对
+export const SITE_TITLE = '康予纳安';
+export const SITE_DESCRIPTION = '欢迎来到康纳的创作宇宙';
+
 export const collections = {
-  blog,
-  vlog,
-  writing,
-  gallery,
-  changelog,
+  blog: 'blog',
+  changelog: 'changelog',
 };
