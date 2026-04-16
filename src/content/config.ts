56a23e45ae09bf5fa0eca7003fcf788f3123c15e
iukconnor0942-sudo <iukconnor0942@gmail.com>
2026-04-16 10:45:45 +0800

    feat: 增加第一篇代数笔记

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,17 @@
 import { defineCollection, z } from 'astro:content';
 
-const notes = defineCollection({
+const notesCollection = defineCollection({
   schema: z.object({
     title: z.string(),
-    description: z.string(),
+    description: z.string().optional(),
     category: z.string().optional(),
     cover: z.string().optional(),
     order: z.number().optional(),
-    // 自动使用 NoteLayout
+    // 自动使用你喜欢的 NoteLayout
     layout: z.string().default('../../layouts/NoteLayout.astro'),
   })
 });
 
-export const collections = { notes };
+export const collections = {
+  notes: notesCollection,
+};
